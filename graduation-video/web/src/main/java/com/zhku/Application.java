8e86e774a5295f484db56eefa8a49481ec071a31
huangshuhong <huangshuhong@do1.com.cn>
2019-12-19 18:06:06 +0800

    2

--- graduation-video/web/src/main/java/com/zhku/Application.java
+++ graduation-video/web/src/main/java/com/zhku/Application.java
@@ -3,10 +3,12 @@ package com.zhku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableDiscoveryClient
 @MapperScan(basePackages="com.zhku.mapper")
 @ComponentScan(basePackages= {"com.zhku", "org.n3r.idworker"})
--- graduation-video/web/src/main/java/com/zhku/controller/HelloWorldController.java
+++ graduation-video/web/src/main/java/com/zhku/controller/HelloWorldController.java
@@ -1,13 +1,25 @@
 package com.zhku.controller;
 
+import com.zhku.message.MessageFeignClient;
+import com.zhku.utils.FeignResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
+@RequestMapping("/hello")
 public class HelloWorldController {
-	
-	@RequestMapping("/hello")
-	public String Hello() {
+
+	@Resource
+	private MessageFeignClient messageFeignClient;
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
+		FeignResult hello = messageFeignClient.hello();
+		System.out.println("hello" + hello);
 		return "Hello Spring Boot~";
 	}
 	
